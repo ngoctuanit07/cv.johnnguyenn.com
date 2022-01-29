7d554c0874081f310263b394baef22c48023e8f5
John Nguyen <ngoctuanit07@gmail.com>
2022-01-29 21:47:17 +0700

    git init

---
+++ precache-manifest.8fb00f0fee933cc1d9e17a58187483f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02fffe32ad3b3e3db7bae29e4cc2caf",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "68b36106990e3eb6ca26",
+    "url": "/mycv/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "c70dd2835d57290f24ea",
+    "url": "/mycv/static/js/2.f95fc29a.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/mycv/static/js/2.f95fc29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b36106990e3eb6ca26",
+    "url": "/mycv/static/js/main.8f4a9e3f.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  }
+]);
